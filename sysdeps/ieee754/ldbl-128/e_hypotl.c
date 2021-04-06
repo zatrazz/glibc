6d7ff766dbb255df102906d9d5e3920a4a0ee98c
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2021-04-05 23:55:55 -0300

    math: Use an improved algorithm for hypotl (ldbl-128)
    This implementation is based on the 'An Improved Algorithm for
    hypot(a,b)' by Carlos F. Borges [1] using the MyHypot3 with the
    following changes:
    
      - Handle qNaN and sNaN
      - Tune the 'widely varying operands' to avoid spurious underflow
        due the multiplication and fix the return value for upwards
        rounding mode.
      - Handle required underflow exception for denormal results.
    
    The main advantage of the new algorithm is its precision.  With a
    random 1e9 input pairs in the range of [LDBL_MIN, LDBL_MAX], glibc
    current implementation shows around 0.05% results with an error of
    1 ulp (453266 results) while the new implementation only shows
    0.0001% of total (1280).
    
    [1] https://arxiv.org/pdf/1904.09481.pdf
    
    Checked on aarch64-linux-gnu and x86_64-linux-gnu.

--- sysdeps/ieee754/ldbl-128/e_hypotl.c
+++ sysdeps/ieee754/ldbl-128/e_hypotl.c
@@ -1,142 +1,110 @@
-/* e_hypotl.c -- long double version of e_hypot.c.
- * Conversion to long double by Jakub Jelinek, jakub@redhat.com.
- */
+/* Euclidean distance function.  Long Double/Binary96 version.
+   Copyright (C) 2021 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
 
-/*
- * ====================================================
- * Copyright (C) 1993 by Sun Microsystems, Inc. All rights reserved.
- *
- * Developed at SunPro, a Sun Microsystems, Inc. business.
- * Permission to use, copy, modify, and distribute this
- * software is freely granted, provided that this notice
- * is preserved.
- * ====================================================
- */
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
 
-/* __ieee754_hypotl(x,y)
- *
- * Method :
- *	If (assume round-to-nearest) z=x*x+y*y
- *	has error less than sqrtl(2)/2 ulp, than
- *	sqrtl(z) has error less than 1 ulp (exercise).
- *
- *	So, compute sqrtl(x*x+y*y) with some care as
- *	follows to get the error below 1 ulp:
- *
- *	Assume x>y>0;
- *	(if possible, set rounding to round-to-nearest)
- *	1. if x > 2y  use
- *		x1*x1+(y*y+(x2*(x+x1))) for x*x+y*y
- *	where x1 = x with lower 64 bits cleared, x2 = x-x1; else
- *	2. if x <= 2y use
- *		t1*y1+((x-y)*(x-y)+(t1*y2+t2*y))
- *	where t1 = 2x with lower 64 bits cleared, t2 = 2x-t1,
- *	y1= y with lower 64 bits chopped, y2 = y-y1.
- *
- *	NOTE: scaling may be necessary if some argument is too
- *	      large or too tiny
- *
- * Special cases:
- *	hypotl(x,y) is INF if x or y is +INF or -INF; else
- *	hypotl(x,y) is NAN if x or y is NAN.
- *
- * Accuracy:
- *	hypotl(x,y) returns sqrtl(x^2+y^2) with error less
- *	than 1 ulps (units in the last place)
- */
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
+/* This implementation is based on the 'An Improved Algorithm for
+   hypot(a,b)' by Carlos F. Borges [1] using the MyHypot3 with the
+   following changes:
+
+   - Handle qNaN and sNaN.
+   - Tune the 'widely varying operands' to avoid spurious underflow
+     due the multiplication and fix the return value for upwards
+     rounding mode.
+   - Handle required underflow exception for denormal results.
+
+   [1] https://arxiv.org/pdf/1904.09481.pdf  */
 
 #include <math.h>
 #include <math_private.h>
 #include <math-underflow.h>
 #include <libm-alias-finite.h>
 
+/* sqrt (LDBL_EPSILON / 2.0)  */
+#define SQRT_EPS_DIV_2     L(0x1.6a09e667f3bcc908b2fb1366ea95p-57)
+/* DBL_MIN / (sqrt (LDBL_EPSILON / 2.0))   */
+#define DBL_MIN_THRESHOLD  L(0x1.6a09e667f3bcc908b2fb1366ea96p-16326)
+/* eps (sqrt (LDBL_MIN))  */
+#define SCALE              L(0x1p-8303)
+/* 1 / eps (sqrt (LDBL_MIN)  */
+#define INV_SCALE          L(0x1p+8303)
+/* sqrt (LDBL_MAX)  */
+#define SQRT_DBL_MAX       L(0x1.6a09e667f3bcc908b2fb1366ea95p+8191)
+/* sqrt (LDBL_MIN)  */
+#define SQRT_DBL_MIN       L(0x1p-8191)
+
 _Float128
 __ieee754_hypotl(_Float128 x, _Float128 y)
 {
-	_Float128 a,b,t1,t2,y1,y2,w;
-	int64_t j,k,ha,hb;
+  if ((isinf (x) || isinf (y))
+      && !issignaling (x) && !issignaling (y))
+    return INFINITY;
+  if (isnan (x) || isnan (y))
+    return x + y;
 
-	GET_LDOUBLE_MSW64(ha,x);
-	ha &= 0x7fffffffffffffffLL;
-	GET_LDOUBLE_MSW64(hb,y);
-	hb &= 0x7fffffffffffffffLL;
-	if(hb > ha) {a=y;b=x;j=ha; ha=hb;hb=j;} else {a=x;b=y;}
-	SET_LDOUBLE_MSW64(a,ha);	/* a <- |a| */
-	SET_LDOUBLE_MSW64(b,hb);	/* b <- |b| */
-	if((ha-hb)>0x78000000000000LL) {return a+b;} /* x/y > 2**120 */
-	k=0;
-	if(ha > 0x5f3f000000000000LL) {	/* a>2**8000 */
-	   if(ha >= 0x7fff000000000000LL) {	/* Inf or NaN */
-	       uint64_t low;
-	       w = a+b;			/* for sNaN */
-	       if (issignaling (a) || issignaling (b))
-		 return w;
-	       GET_LDOUBLE_LSW64(low,a);
-	       if(((ha&0xffffffffffffLL)|low)==0) w = a;
-	       GET_LDOUBLE_LSW64(low,b);
-	       if(((hb^0x7fff000000000000LL)|low)==0) w = b;
-	       return w;
-	   }
-	   /* scale a and b by 2**-9600 */
-	   ha -= 0x2580000000000000LL;
-	   hb -= 0x2580000000000000LL;	k += 9600;
-	   SET_LDOUBLE_MSW64(a,ha);
-	   SET_LDOUBLE_MSW64(b,hb);
-	}
-	if(hb < 0x20bf000000000000LL) {	/* b < 2**-8000 */
-	    if(hb <= 0x0000ffffffffffffLL) {	/* subnormal b or 0 */
-		uint64_t low;
-		GET_LDOUBLE_LSW64(low,b);
-		if((hb|low)==0) return a;
-		t1=0;
-		SET_LDOUBLE_MSW64(t1,0x7ffd000000000000LL); /* t1=2^16382 */
-		b *= t1;
-		a *= t1;
-		k -= 16382;
-		GET_LDOUBLE_MSW64 (ha, a);
-		GET_LDOUBLE_MSW64 (hb, b);
-		if (hb > ha)
-		  {
-		    t1 = a;
-		    a = b;
-		    b = t1;
-		    j = ha;
-		    ha = hb;
-		    hb = j;
-		  }
-	    } else {		/* scale a and b by 2^9600 */
-		ha += 0x2580000000000000LL;	/* a *= 2^9600 */
-		hb += 0x2580000000000000LL;	/* b *= 2^9600 */
-		k -= 9600;
-		SET_LDOUBLE_MSW64(a,ha);
-		SET_LDOUBLE_MSW64(b,hb);
-	    }
-	}
-    /* medium size a and b */
-	w = a-b;
-	if (w>b) {
-	    t1 = 0;
-	    SET_LDOUBLE_MSW64(t1,ha);
-	    t2 = a-t1;
-	    w  = sqrtl(t1*t1-(b*(-b)-t2*(a+t1)));
-	} else {
-	    a  = a+a;
-	    y1 = 0;
-	    SET_LDOUBLE_MSW64(y1,hb);
-	    y2 = b - y1;
-	    t1 = 0;
-	    SET_LDOUBLE_MSW64(t1,ha+0x0001000000000000LL);
-	    t2 = a - t1;
-	    w  = sqrtl(t1*y1-(w*(-w)-(t1*y2+t2*b)));
-	}
-	if(k!=0) {
-	    uint64_t high;
-	    t1 = 1;
-	    GET_LDOUBLE_MSW64(high,t1);
-	    SET_LDOUBLE_MSW64(t1,high+(k<<48));
-	    w *= t1;
-	    math_check_force_underflow_nonneg (w);
-	    return w;
-	} else return w;
+  _Float128 ax = fabsl (x);
+  _Float128 ay = fabsl (y);
+  if (ay > ax)
+    {
+      _Float128 tmp = ax;
+      ax = ay;
+      ay = tmp;
+    }
+
+  /* Widely varying operands.  The DBL_MIN_THRESHOLD check is used to avoid
+     an spurious underflow from the multiplication.  */
+  if (ax >= DBL_MIN_THRESHOLD && ay <= ax * SQRT_EPS_DIV_2)
+    return (ay == 0.0) ? ax : ax + LDBL_DENORM_MIN;
+
+  _Float128 scale = SCALE;
+  if (ax > SQRT_DBL_MAX)
+    {
+      ax *= scale;
+      ay *= scale;
+      scale = INV_SCALE;
+    }
+  else if (ay < SQRT_DBL_MIN)
+    {
+      ax /= scale;
+      ay /= scale;
+    }
+  else
+    scale = 1.0;
+
+  _Float128 h = sqrtl (ax * ax + ay * ay);
+
+  _Float128 t1;
+  _Float128 t2;
+  if (h == 0.0)
+    return h;
+  if (h <= 2.0 * ay)
+    {
+      _Float128 delta = h - ay;
+      t1 = ax * (2.0 * delta - ax);
+      t2 = (delta - 2.0 * (ax - ay)) * delta;
+    }
+  else
+    {
+      _Float128 delta = h - ax;
+      t1 = 2.0 * delta * (ax - 2 * ay);
+      t2 = (4.0 * delta - ay) * ay + delta * delta;
+    }
+  h -= (t1 + t2) / (2.0 * h);
+  h *= scale;
+  math_check_force_underflow_nonneg (h);
+  return h;
 }
 libm_alias_finite (__ieee754_hypotl, __hypotl)
