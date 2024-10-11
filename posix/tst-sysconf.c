d4132e17abf9fb0261a065fbf16031ae9c4e73d8
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2024-10-11 12:31:29 -0300

    posix: Adjust tst-sysconf.c to libsupport

--- posix/tst-sysconf.c
+++ posix/tst-sysconf.c
@@ -1,15 +1,38 @@
+/* Basic tests sysconf.g
+   Copyright (C) XXXX-2024 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#include <array_length.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <unistd.h>
 
-static struct
+#include <support/check.h>
+
+struct sysconf_test_t
 {
   long int _P_val;
   const char *name;
   int _SC_val;
   bool positive;
   bool posix2;
-} posix_options[] =
+};
+
+static struct sysconf_test_t posix_options[] =
   {
 #define N_(name, pos) { _POSIX_##name, #name, _SC_##name, pos, false }
 #define NP(name) N_ (name, true)
@@ -70,14 +93,11 @@ static struct
     N2 (C_DEV),
     N2 (CHAR_TERM)
   };
-#define nposix_options (sizeof (posix_options) / sizeof (posix_options[0]))
 
 static int
 do_test (void)
 {
-  int result = 0;
-
-  for (int i = 0; i < nposix_options; ++i)
+  for (int i = 0; i < array_length (posix_options); ++i)
     {
       long int scret = sysconf (posix_options[i]._SC_val);
 
@@ -85,7 +105,7 @@ do_test (void)
 	{
 	  printf ("sysconf(_SC_%s%s) returned zero\n",
 		  posix_options[i].posix2 ? "2_" : "", posix_options[i].name);
-	  result = 1;
+	  support_record_failure ();
 	}
       if (posix_options[i]._P_val != 0 && posix_options[i]._P_val != scret)
 	{
@@ -94,13 +114,13 @@ do_test (void)
 		  scret,
 		  posix_options[i].posix2 ? "2" : "", posix_options[i].name,
 		  posix_options[i]._P_val);
-	  result = 1;
+	  support_record_failure ();
 	}
       else if (posix_options[i].positive && scret < 0)
 	{
 	  printf ("sysconf(_SC_%s%s) must be > 0\n",
 		  posix_options[i].posix2 ? "2_" : "", posix_options[i].name);
-	  result = 1;
+	  support_record_failure ();
 	}
 
 #define STDVER 200809L
@@ -109,12 +129,11 @@ do_test (void)
 	  printf ("sysconf(_SC_%s%s) must be %ldL\n",
 		  posix_options[i].posix2 ? "2_" : "", posix_options[i].name,
 		  STDVER);
-	  result = 1;
+	  support_record_failure ();
 	}
     }
 
-  return result;
+  return 0;
 }
 
-#define TEST_FUNCTION do_test ()
-#include "../test-skeleton.c"
+#include <support/test-driver.c>
