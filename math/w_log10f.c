b8f79121bbc8929e779afb75a56e767e0c018475
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2024-12-19 17:13:06 -0300

    math: Remove the error handling wrapper from log10f
    It improves latency for about 3-10% and throughput for about 5-10%.

---
+++ math/w_log10f.c
@@ -0,0 +1,8 @@
+#include <math-type-macros-float.h>
+#undef __USE_WRAPPER_TEMPLATE
+#define __USE_WRAPPER_TEMPLATE 1
+#undef declare_mgen_alias
+#define declare_mgen_alias(a, b)
+#include <w_log10_template.c>
+versioned_symbol (libm, __log10f, log10f, GLIBC_2_42);
+libm_alias_float_other (__log10f, log10f)
