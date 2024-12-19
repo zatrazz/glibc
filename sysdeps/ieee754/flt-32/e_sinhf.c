70e1e3981031661e12cd1c712dfdabfba22f4d06
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2024-12-19 17:13:14 -0300

    math: Remove the error handling wrapper from sinhf
    It improves latency for about 3-10% and throughput for about 5-15%.

--- sysdeps/ieee754/flt-32/e_sinhf.c
+++ sysdeps/ieee754/flt-32/e_sinhf.c
@@ -24,14 +24,17 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
+#include <errno.h>
 #include <math.h>
 #include <stdint.h>
 #include <libm-alias-finite.h>
+#include <libm-alias-float.h>
+#include <math-svid-compat.h>
 #include <math-narrow-eval.h>
 #include "math_config.h"
 
 float
-__ieee754_sinhf (float x)
+__sinhf (float x)
 {
   static const double c[] =
     {
@@ -75,6 +78,7 @@ __ieee754_sinhf (float x)
 	  return copysignf (INFINITY, x); /* +-inf */
 	}
       float r = math_narrow_eval (sgn * 0x1.fffffep127f);
+      __set_errno (ERANGE);
       return r;
     }
   if (__glibc_unlikely (ux < 0x7c000000u))
@@ -128,4 +132,11 @@ __ieee754_sinhf (float x)
     }
   return ub;
 }
+strong_alias (__sinhf, __ieee754_sinhf)
+#if LIBM_SVID_COMPAT
+versioned_symbol (libm, __sinhf, sinhf, GLIBC_2_42);
+libm_alias_float_other (__sinh, sinh)
+#else
+libm_alias_float (__sinh, sinh)
+#endif
 libm_alias_finite (__ieee754_sinhf, __sinhf)
