7484e6e9d32a8a1467da833daa037519b8ea1b41
Szabolcs Nagy <szabolcs.nagy@arm.com>
2025-01-14 16:03:23 +0000

    aarch64: Handle GCS marking
     - Handle GCS marking
     - Use l_searchlist.r_list for gcs (allows using the
       same function for static exe)

--- sysdeps/aarch64/dl-gcs.c
+++ sysdeps/aarch64/dl-gcs.c
@@ -15,7 +15,60 @@
    License along with the GNU C Library; if not, see
    <https://www.gnu.org/licenses/>.  */
 
+#include <unistd.h>
+#include <errno.h>
+#include <libintl.h>
 #include <ldsodefs.h>
+
+static void
+fail (struct link_map *l, const char *program)
+{
+  if (program && program[0])
+    _dl_fatal_printf ("%s: %s: %s\n", program, l->l_name, "not GCS compatible");
+  else if (program)
+    _dl_fatal_printf ("%s\n", "not GCS compatible");
+  else
+    _dl_signal_error (0, l->l_name, "dlopen", "not GCS compatible");
+}
+
+static void
+unsupported (void)
+{
+  _dl_fatal_printf ("%s\n", "unsupported GCS policy");
+}
+
+static void
+check_gcs (struct link_map *l, const char *program)
+{
+  bool for_dlopen = program == NULL;
+  if (!l->l_mach.gcs)
+    {
+      if (GLRO(dl_aarch64_gcs_policy) == 2 || for_dlopen)
+	fail (l, program);
+      if (GLRO(dl_aarch64_gcs_policy) == 1)
+	GL(dl_aarch64_gcs) = 0;
+      else
+	unsupported ();
+    }
+}
+
+/* Apply GCS policy for L and its dependencies.
+   PROGRAM is NULL when this check is invoked for dl_open.  */
+
+void
+_dl_gcs_check (struct link_map *l, const char *program)
+{
+  /* GCS is disabled.  */
+  if (GL(dl_aarch64_gcs) == 0)
+    return;
+  /* GCS marking is ignored.  */
+  if (GLRO(dl_aarch64_gcs_policy) == 0)
+    return;
+
+  check_gcs (l, program);
+  for (unsigned int i = 0; i < l->l_searchlist.r_nlist; i++)
+    check_gcs (l->l_searchlist.r_list[i], program);
+}
 
 /* Used to report error when prctl system call to enabled GCS fails.  */
 
