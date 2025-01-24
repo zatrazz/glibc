b04e19b8397cdb35f6a56cb9e40a91b65e89d6da
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2025-01-24 16:04:06 -0300

    math: Consolidate cospif and sinpif internal tables
    The libm size improvement on aarch64 with --enable-stack-protector=no,
    --enable-bind-now=yes, --enable-profile=yes, --enable-fortify-source=2,
    and --enable-cet is:
    
    aarch64-linux-gnu $ size math/libm.so
       text    data     bss     dec     hex filename
     632252     844      12  633108   9a914 math/libm.so
    
    aarch64-linux-gnu $ size math/libm.so
       text    data     bss     dec     hex filename
     631196     844      12  632052   9a4f4 math/libm.so

--- sysdeps/ieee754/flt-32/s_sinpif.c
+++ sysdeps/ieee754/flt-32/s_sinpif.c
@@ -30,65 +30,11 @@ SOFTWARE.
 #include <fenv.h>
 #include <libm-alias-float.h>
 #include "math_config.h"
+#include "s_sincospif.h"
 
 float
 __sinpif (float x)
 {
-  static const double sn[] =
-    {
-      0x1.921fb54442d0fp-37, -0x1.4abbce6102b94p-112, 0x1.4669fa3c58463p-189
-    };
-  static const double cn[] =
-    {
-      -0x1.3bd3cc9be45cfp-74, 0x1.03c1f08088742p-150, -0x1.55d1e5eff55a5p-228
-    };
-  static const double S[] =
-    {
-       0x0p+0,                 0x1.91f65f10dd814p-5,    0x1.917a6bc29b42cp-4,
-       0x1.2c8106e8e613ap-3,   0x1.8f8b83c69a60bp-3,    0x1.f19f97b215f1bp-3,
-       0x1.294062ed59f06p-2,   0x1.58f9a75ab1fddp-2,    0x1.87de2a6aea963p-2,
-       0x1.b5d1009e15ccp-2,    0x1.e2b5d3806f63bp-2,    0x1.073879922ffeep-1,
-       0x1.1c73b39ae68c8p-1,   0x1.30ff7fce17035p-1,    0x1.44cf325091dd6p-1,
-       0x1.57d69348cecap-1,    0x1.6a09e667f3bcdp-1,    0x1.7b5df226aafafp-1,
-       0x1.8bc806b151741p-1,   0x1.9b3e047f38741p-1,    0x1.a9b66290ea1a3p-1,
-       0x1.b728345196e3ep-1,   0x1.c38b2f180bdb1p-1,    0x1.ced7af43cc773p-1,
-       0x1.d906bcf328d46p-1,   0x1.e212104f686e5p-1,    0x1.e9f4156c62ddap-1,
-       0x1.f0a7efb9230d7p-1,   0x1.f6297cff75cbp-1,     0x1.fa7557f08a517p-1,
-       0x1.fd88da3d12526p-1,   0x1.ff621e3796d7ep-1,    0x1p+0,
-       0x1.ff621e3796d7ep-1,   0x1.fd88da3d12526p-1,    0x1.fa7557f08a517p-1,
-       0x1.f6297cff75cbp-1,    0x1.f0a7efb9230d7p-1,    0x1.e9f4156c62ddap-1,
-       0x1.e212104f686e5p-1,   0x1.d906bcf328d46p-1,    0x1.ced7af43cc773p-1,
-       0x1.c38b2f180bdb1p-1,   0x1.b728345196e3ep-1,    0x1.a9b66290ea1a3p-1,
-       0x1.9b3e047f38741p-1,   0x1.8bc806b151741p-1,    0x1.7b5df226aafafp-1,
-       0x1.6a09e667f3bcdp-1,   0x1.57d69348cecap-1,     0x1.44cf325091dd6p-1,
-       0x1.30ff7fce17035p-1,   0x1.1c73b39ae68c8p-1,    0x1.073879922ffeep-1,
-       0x1.e2b5d3806f63bp-2,   0x1.b5d1009e15ccp-2,     0x1.87de2a6aea963p-2,
-       0x1.58f9a75ab1fddp-2,   0x1.294062ed59f06p-2,    0x1.f19f97b215f1bp-3,
-       0x1.8f8b83c69a60bp-3,   0x1.2c8106e8e613ap-3,    0x1.917a6bc29b42cp-4,
-       0x1.91f65f10dd814p-5,   0x0p+0,                 -0x1.91f65f10dd814p-5,
-      -0x1.917a6bc29b42cp-4,  -0x1.2c8106e8e613ap-3,   -0x1.8f8b83c69a60bp-3,
-      -0x1.f19f97b215f1bp-3,  -0x1.294062ed59f06p-2,   -0x1.58f9a75ab1fddp-2,
-      -0x1.87de2a6aea963p-2,  -0x1.b5d1009e15ccp-2,    -0x1.e2b5d3806f63bp-2,
-      -0x1.073879922ffeep-1,  -0x1.1c73b39ae68c8p-1,   -0x1.30ff7fce17035p-1,
-      -0x1.44cf325091dd6p-1,  -0x1.57d69348cecap-1,    -0x1.6a09e667f3bcdp-1,
-      -0x1.7b5df226aafafp-1,  -0x1.8bc806b151741p-1,   -0x1.9b3e047f38741p-1,
-      -0x1.a9b66290ea1a3p-1,  -0x1.b728345196e3ep-1,   -0x1.c38b2f180bdb1p-1,
-      -0x1.ced7af43cc773p-1,  -0x1.d906bcf328d46p-1,   -0x1.e212104f686e5p-1,
-      -0x1.e9f4156c62ddap-1,  -0x1.f0a7efb9230d7p-1,   -0x1.f6297cff75cbp-1,
-      -0x1.fa7557f08a517p-1,  -0x1.fd88da3d12526p-1,   -0x1.ff621e3796d7ep-1,
-      -0x1p+0,                -0x1.ff621e3796d7ep-1,   -0x1.fd88da3d12526p-1,
-      -0x1.fa7557f08a517p-1,  -0x1.f6297cff75cbp-1,    -0x1.f0a7efb9230d7p-1,
-      -0x1.e9f4156c62ddap-1,  -0x1.e212104f686e5p-1,   -0x1.d906bcf328d46p-1,
-      -0x1.ced7af43cc773p-1,  -0x1.c38b2f180bdb1p-1,   -0x1.b728345196e3ep-1,
-      -0x1.a9b66290ea1a3p-1,  -0x1.9b3e047f38741p-1,   -0x1.8bc806b151741p-1,
-      -0x1.7b5df226aafafp-1,  -0x1.6a09e667f3bcdp-1,   -0x1.57d69348cecap-1,
-      -0x1.44cf325091dd6p-1,  -0x1.30ff7fce17035p-1,   -0x1.1c73b39ae68c8p-1,
-      -0x1.073879922ffeep-1,  -0x1.e2b5d3806f63bp-2,   -0x1.b5d1009e15ccp-2,
-      -0x1.87de2a6aea963p-2,  -0x1.58f9a75ab1fddp-2,   -0x1.294062ed59f06p-2,
-      -0x1.f19f97b215f1bp-3,  -0x1.8f8b83c69a60bp-3,   -0x1.2c8106e8e613ap-3,
-      -0x1.917a6bc29b42cp-4,  -0x1.91f65f10dd814p-5
-    };
-
   uint32_t ix = asuint (x);
   int32_t e = (ix >> 23) & 0xff;
   if (__glibc_unlikely (e == 0xff))
@@ -109,7 +55,7 @@ __sinpif (float x)
       iq &= 127;
       if (iq == 0 || iq == 64)
 	return copysignf (0.0f, x);
-      return S[iq];
+      return __sincospif_S[iq];
     }
   else if (__glibc_unlikely (s > 30))
     {
@@ -122,12 +68,14 @@ __sinpif (float x)
 
   int32_t k = m << (31 - s);
   double z = k, z2 = z * z;
-  double fs = sn[0] + z2 * (sn[1] + z2 * sn[2]);
-  double fc = cn[0] + z2 * (cn[1] + z2 * cn[2]);
+  double fs = __sincospif_sn[0]
+    + z2 * (__sincospif_sn[1] + z2 * __sincospif_sn[2]);
+  double fc = __sincospif_cn[0]
+    + z2 * (__sincospif_cn[1] + z2 * __sincospif_cn[2]);
   uint32_t iq = m >> s;
   iq = (iq + 1) >> 1;
   uint32_t is = iq & 127, ic = (iq + 32) & 127;
-  double ts = S[is], tc = S[ic];
+  double ts = __sincospif_S[is], tc = __sincospif_S[ic];
   double r = ts + (ts * z2) * fc + (tc * z) * fs;
   return r;
 }
