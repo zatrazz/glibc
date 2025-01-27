b719f770304f55f67aeadfb13b2a629c7bfcf1c4
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2025-01-27 15:25:18 -0300

    math: Consolidate acosf and asinf internal tables
    The libm size improvement built with "--enable-stack-protector=strong
    --enable-bind-now=yes --enable-fortify-source=2":
    
    From:
    
       text	   data	    bss	    dec	    hex	filename
     587896     860      12  588768   8fbe0 aarch64-linux-gnu-master/math/libm.so
     963175    1068      12  964255   eb69f x86_64-linux-gnu-master/math/libm.so
    1191542    5544     368 1197454  12458e powerpc64le-linux-gnu-master/math/libm.so
    
    To:
    
       text	   data	    bss	    dec	    hex	filename
     587304     860      12  588176   8f990 aarch64-linux-gnu/math/libm.so
     962855    1068      12  963935   eb55f x86_64-linux-gnu/math/libm.so
    1191222    5544     368 1197134  12444e powerpc64le-linux-gnu/math/libm.so
    
    The are not code changes for x86_64 and powerpc64le, but on aarch64
    with gcc-14 I see a slight better code generation due the usage of
    ldq for floating point constant loading.

--- sysdeps/ieee754/flt-32/e_acosf.c
+++ sysdeps/ieee754/flt-32/e_acosf.c
@@ -32,6 +32,7 @@ SOFTWARE.
 #include <libm-alias-float.h>
 #include <math-svid-compat.h>
 #include "math_config.h"
+#include "e_asincosf_data.h"
 
 static __attribute__ ((noinline)) float
 as_special (float x)
@@ -79,15 +80,6 @@ __acosf (float x)
     return as_special (x);
   if (__glibc_likely (ax < 0x7ec2a1dcu)) /* |x| < 0x1.c2a1dcp-1 */
     {
-      static const double b[] =
-	{
-	   0x1.fffffffd9ccb8p-1,  0x1.5555c94838007p-3,  0x1.32ded4b7c20fap-4,
-	   0x1.8566df703309ep-5, -0x1.980c959bec9a3p-6,  0x1.56fbb04998344p-1,
-	  -0x1.403d8e4c49f52p+2,  0x1.b06c3e9f311eap+4, -0x1.9ea97c4e2c21fp+6,
-	   0x1.200b8261cc61bp+8, -0x1.2274c2799a5c7p+9,  0x1.a558a59cc19d3p+9,
-	  -0x1.aca4b6a529ffp+9,   0x1.228744703f813p+9, -0x1.d7dbb0b322228p+7,
-	   0x1.5c2018c0c0105p+5
-	};
       /* Avoid spurious underflow exception.  */
       if (__glibc_unlikely (ax <= 0x40000000u)) /* |x| < 2^-63 */
 	/* GCC <= 11 wrongly assumes the rounding is to nearest and
@@ -99,11 +91,11 @@ __acosf (float x)
       double z4 = z2 * z2;
       double z8 = z4 * z4;
       double z16 = z8 * z8;
-      r = z * ((((b[0] + z2 * b[1]) + z4 * (b[2] + z2 * b[3]))
-		+ z8 * ((b[4] + z2 * b[5]) + z4 * (b[6] + z2 * b[7])))
-	       + z16 * (((b[8] + z2 * b[9]) + z4 * (b[10] + z2 * b[11]))
+      r = z * ((((B[0] + z2 * B[1]) + z4 * (B[2] + z2 * B[3]))
+		+ z8 * ((B[4] + z2 * B[5]) + z4 * (B[6] + z2 * B[7])))
+	       + z16 * (((B[8] + z2 * B[9]) + z4 * (B[10] + z2 * B[11]))
 			+ z8
-			* ((b[12] + z2 * b[13])+ z4 * (b[14] + z2 * b[15]))));
+			* ((B[12] + z2 * B[13])+ z4 * (B[14] + z2 * B[15]))));
       float ub = 0x1.921fb54574191p+0 - r;
       float lb = 0x1.921fb543118ap+0 - r;
       if (ub == lb)
@@ -112,33 +104,19 @@ __acosf (float x)
   /* accurate path  */
   if (ax < (0x7eu << 24))
     {
-      static const double c[] =
-	{
-	  0x1.555555555529cp-3,  0x1.333333337e0ddp-4, 0x1.6db6db3b4465ep-5,
-	  0x1.f1c72e13ac306p-6,  0x1.6e89cebe06bc4p-6, 0x1.1c6dcf5289094p-6,
-	  0x1.c6dbbcc7c6315p-7,  0x1.8f8dc2615e996p-7, 0x1.a5833b7bf15e8p-8,
-	  0x1.43f44ace1665cp-6, -0x1.0fb17df881c73p-6, 0x1.07520c026b2d6p-5
-	};
       if (t == 0x328885a3u)
 	return 0x1.921fb6p+0f + 0x1p-25;
       if (t == 0x39826222u)
 	return 0x1.920f6ap+0f + 0x1p-25;
       double x2 = xs * xs;
-      r = (pi2 - xs) - (xs * x2) * poly12 (x2, c);
+      r = (pi2 - xs) - (xs * x2) * poly12 (x2, C0);
     }
   else
     {
-      static const double c[] =
-	{
-	  0x1.6a09e667f3bcbp+0,   0x1.e2b7dddff2db9p-4,  0x1.b27247ab42dbcp-6,
-	  0x1.02995cc4e0744p-7,   0x1.5ffb0276ec8eap-9,  0x1.033885a928decp-10,
-	  0x1.911f2be23f8c7p-12,  0x1.4c3c55d2437fdp-13, 0x1.af477e1d7b461p-15,
-	  0x1.abd6bdff67dcbp-15, -0x1.1717e86d0fa28p-16, 0x1.6ff526de46023p-16
-	};
       double bx = fabs (xs);
       double z = 1.0 - bx;
       double s = copysign (sqrt (z), xs);
-      r = o[t >> 31] + s * poly12 (z, c);
+      r = o[t >> 31] + s * poly12 (z, C1);
     }
   return r;
 }
