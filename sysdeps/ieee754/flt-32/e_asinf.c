b7582867ecc48358cdf5ef7125cc20a00356658e
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2025-01-27 15:25:18 -0300

    math: Consolidate acosf and asinf internal tables
    The libm size improvement built with --enable-stack-protector=no,
    --enable-bind-now=yes, --enable-profile=yes, --enable-fortify-source=2:
    
    From:
    
       text	   data	    bss	    dec	    hex	filename
     580991	    812	     12	 581815	  8e0b7	aarch64-linux-gnu/math/libm.so
     962048	   1068	     12	 963128	  eb238	x86_64-linux-gnu/math/libm.so
    1191099	   5544	    360	1197003	 1243cb	powerpc64le-linux-gnu/math/libm.so
    
    To:
    
       text	   data	    bss	    dec	    hex	filename
     580383	    812	     12	 581207	  8de57	aarch64-linux-gnu/math/libm.so
     961728	   1068	     12	 962808	  eb0f8	x86_64-linux-gnu/math/libm.so
    1190787	   5544	    360	1196691	 124293	powerpc64le-linux-gnu/math/libm.so

--- sysdeps/ieee754/flt-32/e_asinf.c
+++ sysdeps/ieee754/flt-32/e_asinf.c
@@ -28,6 +28,7 @@ SOFTWARE.
 #include <errno.h>
 #include <libm-alias-finite.h>
 #include "math_config.h"
+#include "e_asincosf_data.h"
 
 static __attribute__ ((noinline)) float
 as_special (float x)
@@ -69,25 +70,20 @@ __ieee754_asinf (float x)
     {
       if (__glibc_unlikely (ax < 115 << 24))
 	return fmaf (x, 0x1p-25, x);
-      static const double b[] =
-	{
-	   0x1.0000000000005p+0,  0x1.55557aeca105dp-3,  0x1.3314ec3db7d12p-4,
-	   0x1.775738a5a6f92p-5,  0x1.5d5f7ce1c8538p-8,  0x1.605c6d58740fp-2,
-	  -0x1.5728b732d73c6p+1,  0x1.f152170f151ebp+3, -0x1.f962ea3ca992ep+5,
-	   0x1.71971e17375ap+7,  -0x1.860512b4ba23p+8,   0x1.26a3b8d4bdb14p+9,
-	  -0x1.36f2ea5698b51p+9,  0x1.b3d722aebfa2ep+8, -0x1.6cf89703b1289p+7,
-	   0x1.1518af6a65e2dp+5
-	};
       double z = xs;
       double z2 = z * z;
       double z4 = z2 * z2;
       double z8 = z4 * z4;
       double z16 = z8 * z8;
-      r = z * ((((b[0] + z2 * b[1]) + z4 * (b[2] + z2 * b[3]))
-		+ z8 * ((b[4] + z2 * b[5]) + z4 * (b[6] + z2 * b[7])))
-	       + z16 * (((b[8] + z2 * b[9]) + z4 * (b[10] + z2 * b[11]))
-			+ z8 * ((b[12] + z2 * b[13])
-				+ z4 * (b[14] + z2 * b[15]))));
+      r = z * ((((__asincosf_b[0] + z2 * __asincosf_b[1]) + z4
+		 * (__asincosf_b[2] + z2 * __asincosf_b[3]))
+		+ z8 * ((__asincosf_b[4] + z2 * __asincosf_b[5]) + z4
+			* (__asincosf_b[6] + z2 * __asincosf_b[7])))
+	       + z16 * (((__asincosf_b[8] + z2 * __asincosf_b[9]) + z4
+			 * (__asincosf_b[10] + z2 * __asincosf_b[11]))
+			+ z8 * ((__asincosf_b[12] + z2 * __asincosf_b[13])
+				+ z4 * (__asincosf_b[14] + z2
+					* __asincosf_b[15]))));
       float ub = r;
       float lb = r - z * 0x1.efa8ebp-31;
       if (ub == lb)
@@ -95,16 +91,9 @@ __ieee754_asinf (float x)
     }
   if (ax < (0x7eu << 24))
     {
-      static const double c[] =
-	{
-	  0x1.555555555529cp-3,  0x1.333333337e0ddp-4, 0x1.6db6db3b4465ep-5,
-	  0x1.f1c72e13ac306p-6,  0x1.6e89cebe06bc4p-6, 0x1.1c6dcf5289094p-6,
-	  0x1.c6dbbcc7c6315p-7,  0x1.8f8dc2615e996p-7, 0x1.a5833b7bf15e8p-8,
-	  0x1.43f44ace1665cp-6, -0x1.0fb17df881c73p-6, 0x1.07520c026b2d6p-5
-	};
       double z = xs;
       double z2 = z * z;
-      double c0 = poly12 (z2, c);
+      double c0 = poly12 (z2, __asincosf_c0);
       r = z + (z * z2) * c0;
     }
   else
@@ -116,14 +105,7 @@ __ieee754_asinf (float x)
       double bx = fabs (xs);
       double z = 1.0 - bx;
       double s = sqrt (z);
-      static const double c[] =
-	{
-	  0x1.6a09e667f3bcbp+0,   0x1.e2b7dddff2db9p-4,  0x1.b27247ab42dbcp-6,
-	  0x1.02995cc4e0744p-7,   0x1.5ffb0276ec8eap-9,  0x1.033885a928decp-10,
-	  0x1.911f2be23f8c7p-12,  0x1.4c3c55d2437fdp-13, 0x1.af477e1d7b461p-15,
-	  0x1.abd6bdff67dcbp-15, -0x1.1717e86d0fa28p-16, 0x1.6ff526de46023p-16
-	};
-      r = pi2 - s * poly12 (z, c);
+      r = pi2 - s * poly12 (z, __asincosf_c1);
       r = copysign (r, xs);
     }
   return r;
