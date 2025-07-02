cc0bec818e3592897d5668e9011a2bee96614268
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2025-07-02 16:37:08 -0300

    math: Consolidate cosh and sinh internal table

---
+++ sysdeps/ieee754/dbl-64/e_sincosh_data.h
@@ -0,0 +1,36 @@
+/* Common data for hyperbolic sine/cosine for binary64.
+
+Copyright (c) 2023 Alexei Sibidanov.
+
+This file is part of the CORE-MATH project
+(https://core-math.gitlabpages.inria.fr/).
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#ifndef _E_SINCOSH_DATA_H
+#define _E_SINCOSH_DATA_H
+
+extern const double __sincosh_t0[][2] attribute_hidden;
+extern const double __sincosh_t1[][2] attribute_hidden;
+
+#define T0 __sincosh_t0
+#define T1 __sincosh_t1
+
+#endif
