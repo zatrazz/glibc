196725b533cc52d284193227efc8407d594cb28e
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2025-08-12 20:59:18 -0300

    math: Use logf from CORE-MATH

--- sysdeps/ieee754/flt-32/e_logf.c
+++ sysdeps/ieee754/flt-32/e_logf.c
@@ -1,90 +1,151 @@
-/* Single-precision log function.
-   Copyright (C) 2017-2025 Free Software Foundation, Inc.
-   This file is part of the GNU C Library.
+/* Correctly-rounded logarithm function for binary32 value.
 
-   The GNU C Library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public
-   License as published by the Free Software Foundation; either
-   version 2.1 of the License, or (at your option) any later version.
+Copyright (c) 2023-2024 Alexei Sibidanov and Paul Zimmermann.
 
-   The GNU C Library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
+This file is part of the CORE-MATH project
+(https://core-math.gitlabpages.inria.fr/).
 
-   You should have received a copy of the GNU Lesser General Public
-   License along with the GNU C Library; if not, see
-   <https://www.gnu.org/licenses/>.  */
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 
 #include <math.h>
 #include <stdint.h>
+#include <stdbit.h>
 #include <libm-alias-finite.h>
 #include <libm-alias-float.h>
 #include "math_config.h"
 
-/*
-LOGF_TABLE_BITS = 4
-LOGF_POLY_ORDER = 4
-
-ULP error: 0.818 (nearest rounding.)
-Relative error: 1.957 * 2^-26 (before rounding.)
-*/
-
-#define T __logf_data.tab
-#define A __logf_data.poly
-#define Ln2 __logf_data.ln2
-#define N (1 << LOGF_TABLE_BITS)
-#define OFF 0x3f330000
+static __attribute__ ((noinline)) float
+as_special (float x)
+{
+  uint32_t t = asuint (x);
+  uint32_t ux = t;
+  if (ux == 0u)
+    return __math_divzerof (1);
+  if (ux == 0x7f800000u)
+    return x; // +inf
+  uint32_t ax = ux << 1;
+  if (ax == 0u)
+    return __math_divzerof (1);
+  if (ax > 0xff000000u)
+    return x + x; // nan
+  return __math_invalidf (x);
+}
 
 float
 __logf (float x)
 {
-  /* double_t for better performance on targets with FLT_EVAL_METHOD==2.  */
-  double_t z, r, r2, y, y0, invc, logc;
-  uint32_t ix, iz, tmp;
-  int k, i;
-
-  ix = asuint (x);
-#if WANT_ROUNDING
-  /* Fix sign of zero with downward rounding when x==1.  */
-  if (__glibc_unlikely (ix == 0x3f800000))
-    return 0;
-#endif
-  if (__glibc_unlikely (ix - 0x00800000 >= 0x7f800000 - 0x00800000))
+  static const double tr[]
+    = { 0x1p+0,         0x1.f81f82p-1,  0x1.f07c1fp-1,  0x1.e9131acp-1,
+	0x1.e1e1e1ep-1, 0x1.dae6077p-1, 0x1.d41d41dp-1, 0x1.cd85689p-1,
+	0x1.c71c71cp-1, 0x1.c0e0704p-1, 0x1.bacf915p-1, 0x1.b4e81b5p-1,
+	0x1.af286bdp-1, 0x1.a98ef6p-1,  0x1.a41a41ap-1, 0x1.9ec8e95p-1,
+	0x1.999999ap-1, 0x1.948b0fdp-1, 0x1.8f9c19p-1,  0x1.8acb90fp-1,
+	0x1.8618618p-1, 0x1.8181818p-1, 0x1.7d05f41p-1, 0x1.78a4c81p-1,
+	0x1.745d174p-1, 0x1.702e05cp-1, 0x1.6c16c17p-1, 0x1.6816817p-1,
+	0x1.642c859p-1, 0x1.605816p-1,  0x1.5c9882cp-1, 0x1.58ed231p-1,
+	0x1.5555555p-1, 0x1.51d07ebp-1, 0x1.4e5e0a7p-1, 0x1.4afd6ap-1,
+	0x1.47ae148p-1, 0x1.446f865p-1, 0x1.4141414p-1, 0x1.3e22cbdp-1,
+	0x1.3b13b14p-1, 0x1.3813814p-1, 0x1.3521cfbp-1, 0x1.323e34ap-1,
+	0x1.2f684bep-1, 0x1.2c9fb4ep-1, 0x1.29e412ap-1, 0x1.27350b9p-1,
+	0x1.2492492p-1, 0x1.21fb781p-1, 0x1.1f7047ep-1, 0x1.1cf06aep-1,
+	0x1.1a7b961p-1, 0x1.1811812p-1, 0x1.15b1e5fp-1, 0x1.135c811p-1,
+	0x1.1111111p-1, 0x1.0ecf56cp-1, 0x1.0c9715p-1,  0x1.0a6810ap-1,
+	0x1.0842108p-1, 0x1.0624dd3p-1,  0x1.041041p-1, 0x1.0204081p-1,
+	0.5 };
+  static const double tl[]
+      = { -0x1.3b40815cd0628p-45, 0x1.fc0a890fbb514p-7, 0x1.f829b1e780b98p-6,
+	  0x1.77458f532c948p-5,	  0x1.f0a30c2114ef2p-5, 0x1.341d793bbc7f7p-4,
+	  0x1.6f0d28d256172p-4,	  0x1.a926d3a6acb89p-4, 0x1.e2707722ae90cp-4,
+	  0x1.0d77e7a90896cp-3,	  0x1.29552f6fff036p-3, 0x1.44d2b6c5b7831p-3,
+	  0x1.5ff306ee78ee7p-3,	  0x1.7ab890410d41cp-3, 0x1.9525a9e3451c7p-3,
+	  0x1.af3c94ed0bb06p-3,	  0x1.c8ff7c59a9535p-3, 0x1.e27076d5aedf9p-3,
+	  0x1.fb9186b5e393ep-3,	  0x1.0a324e38b8e6dp-2, 0x1.1675cacaba398p-2,
+	  0x1.22941fc0f76efp-2,	  0x1.2e8e2bc311abap-2, 0x1.3a64c56b14373p-2,
+	  0x1.4618bc31c5c4cp-2,	  0x1.51aad874df5b7p-2, 0x1.5d1bdbea80754p-2,
+	  0x1.686c81d331238p-2,	  0x1.739d7f6dbcd9p-2,	0x1.7eaf83c82ad4dp-2,
+	  0x1.89a3385813fe4p-2,	  0x1.947941aa91484p-2, 0x1.9f323edbf95d5p-2,
+	  0x1.a9cec9a4205d3p-2,	  0x1.b44f77c5c8cecp-2, 0x1.beb4d9ea71905p-2,
+	  0x1.c8ff7c69a97abp-2,	  0x1.d32fe7f38e95fp-2, 0x1.dd46a0501c22ap-2,
+	  0x1.e7442617e8511p-2,	  0x1.f128f5eaf0476p-2, 0x1.faf588dd8f0a8p-2,
+	  0x1.02552a5edcfc4p-1,	  0x1.0723e5c64de05p-1, 0x1.0be72e3852947p-1,
+	  0x1.109f39d554b5cp-1,	  0x1.154c3d2c4d4aep-1, 0x1.19ee6b38bc834p-1,
+	  0x1.1e85f5ef03f95p-1,	  0x1.23130d7fabe07p-1, 0x1.2795e1219afep-1,
+	  0x1.2c0e9ec9c8d5p-1,	  0x1.307d7337f0f83p-1, 0x1.34e289cb4e098p-1,
+	  0x1.393e0d42e28dep-1,	  0x1.3d9026ad555bfp-1, 0x1.41d8fe8667173p-1,
+	  0x1.4618bc1ec5d87p-1,	  0x1.4a4f85d303d8p-1,	0x1.4e7d8127f5a75p-1,
+	  0x1.52a2d26dbc47p-1,	  0x1.56bf9d597f25ep-1, 0x1.5ad404cb59df2p-1,
+	  0x1.5ee02a928153ap-1,	  0x1.62e42fefa38b4p-1 };
+  static const double b[]
+      = { 0x1.00000006342eap+0, -0x1.0001f7fdc3977p-1, 0x1.554a4e5cae9cfp-2 };
+  static const double c[] = { -0x1p-1,
+			      0x1.55555555571cap-2,
+			      -0x1.0000000002d85p-2,
+			      0x1.9999987d0c963p-3,
+			      -0x1.555554059a8bbp-3,
+			      0x1.24aebcf71a38fp-3,
+			      -0x1.001c73915d758p-3 };
+  uint32_t ux = asuint (x);
+  if (__glibc_unlikely (ux < (1 << 23) || ux >= 0x7f800000u))
     {
-      /* x < 0x1p-126 or inf or nan.  */
-      if (ix * 2 == 0)
-	return __math_divzerof (1);
-      if (ix == 0x7f800000) /* log(inf) == inf.  */
-	return x;
-      if ((ix & 0x80000000) || ix * 2 >= 0xff000000)
-	return __math_invalidf (x);
-      /* x is subnormal, normalize it.  */
-      ix = asuint (x * 0x1p23f);
-      ix -= 23 << 23;
+      if (ux == 0 || ux >= 0x7f800000u)
+	return as_special (x); // <=0, nan, inf
+      // subnormal
+      int n = stdc_leading_zeros (ux) - 8;
+      ux <<= n;
+      ux -= n << 23;
     }
-
-  /* x = 2^k z; where z is in range [OFF,2*OFF] and exact.
-     The range is split into N subintervals.
-     The ith subinterval contains z and c is near its center.  */
-  tmp = ix - OFF;
-  i = (tmp >> (23 - LOGF_TABLE_BITS)) % N;
-  k = (int32_t) tmp >> 23; /* arithmetic shift */
-  iz = ix - (tmp & 0xff800000);
-  invc = T[i].invc;
-  logc = T[i].logc;
-  z = (double_t) asfloat (iz);
-
-  /* log(x) = log1p(z/c-1) + log(c) + k*Ln2 */
-  r = z * invc - 1;
-  y0 = logc + (double_t) k * Ln2;
-
-  /* Pipelined polynomial evaluation to approximate log1p(r).  */
-  r2 = r * r;
-  y = A[1] * r + A[2];
-  y = A[0] * r2 + y;
-  y = y * r2 + (y0 + r);
-  return (float) y;
+  if (__glibc_unlikely (ux == 127u << 23))
+    return 0.0f;
+  uint32_t m = ux & ((1 << 23) - 1), j = (m + (1 << (23 - 7))) >> (23 - 6);
+  int32_t e = ((int32_t) ux >> 23) - 127;
+  double tz = asdouble (((uint64_t) m | ((int64_t) 1023 << 23)) << (52 - 23));
+  double z = tz * tr[j] - 1, z2 = z * z;
+  double r = ((e * 0x1.62e42fefa39efp-1 + tl[j]) + z * b[0])
+	     + z2 * (b[1] + z * b[2]);
+  float ub = r, lb = r + 0x1.f06p-33;
+  if (__glibc_unlikely (ub != lb))
+    {
+      double f = z2
+		 * ((c[0] + z * c[1])
+		    + z2
+			  * ((c[2] + z * c[3])
+			     + z2 * (c[4] + z * c[5] + z2 * c[6])));
+      if (__glibc_unlikely (fabsf (x - 1.0f) < 0x1p-10f))
+	{
+	  return z + f;
+	}
+      f -= 0x1.0ca86c3898dp-49 * e;
+      f += z;
+      f += tl[j] - tl[0];
+      double el = e * 0x1.62e42fefa3ap-1;
+      r = el + f;
+      ub = r;
+      tz = r;
+      if (__glibc_unlikely (!(asuint64 (tz) & ((1u << 28) - 1u))))
+	{
+	  double dr = (el - r) + f;
+	  r += dr * 64.0;
+	  ub = r;
+	}
+    }
+  return ub;
 }
 #ifndef __logf
 strong_alias (__logf, __ieee754_logf)
