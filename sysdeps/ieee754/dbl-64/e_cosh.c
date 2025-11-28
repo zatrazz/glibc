308f2d4e1556b69c0480b1e14754ddbb74878d50
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2025-11-28 17:18:42 -0300

    x86_64: Add cosh with FMA
    The cosh shows an improvement of about ~35% when building for
    x86_64-v3.

--- sysdeps/ieee754/dbl-64/e_cosh.c
+++ sysdeps/ieee754/dbl-64/e_cosh.c
@@ -35,6 +35,10 @@ SOFTWARE.
 #include "e_coshsinh_data.h"
 #include "math_config.h"
 
+#ifndef SECTION
+#  define SECTION
+#endif
+
 static __attribute__ ((noinline)) double as_cosh_database (double, double);
 
 static double __attribute__ ((noinline))
@@ -82,6 +86,7 @@ as_cosh_database (double x, double f)
   return f;
 }
 
+SECTION
 double
 __cosh (double x)
 {
@@ -223,11 +228,13 @@ __cosh (double x)
     return as_cosh_database (x, rh);
   return rh;
 }
+#ifndef __cosh
 strong_alias (__cosh, __ieee754_cosh)
-#if LIBM_SVID_COMPAT
+# if LIBM_SVID_COMPAT
 versioned_symbol (libm, __cosh, cosh, GLIBC_2_43);
 libm_alias_double_other (__cosh, cosh)
-#else
+# else
 libm_alias_double (__cosh, cosh)
-#endif
+# endif
 libm_alias_finite (__ieee754_cosh, __cosh)
+#endif
