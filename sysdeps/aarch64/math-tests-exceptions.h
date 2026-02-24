92ecd9a5dbea0ef36f7c00dbd4bd7aea3680e927
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2026-02-24 15:55:24 -0300

    math: Disable exception check for soft-fp routine with compiler-rt
    The compiler-rt does not support throwing floating-point exceptions
    for math builtin (__addtf3, __multf3, __divtf3, etc.) [1][2][3].
    So disable the check when compiler-rt is used instead of libgcc.
    
    Checked on x86_64-linux-gnu and aarch64-linux-gnu with clang.
    
    [1] https://github.com/llvm/llvm-project/issues/59924
    [2] https://github.com/llvm/llvm-project/issues/60141
    [3] https://github.com/llvm/llvm-project/issues/172499

---
+++ sysdeps/aarch64/math-tests-exceptions.h
@@ -0,0 +1,34 @@
+/* Configuration for math tests: support for exceptions.  AArch64 version.
+   Copyright (C) 2026 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#ifndef _MATH_TESTS_EXCEPTIONS_H
+#define _MATH_TESTS_EXCEPTIONS_H 1
+
+#define EXCEPTION_TESTS_float	1
+#define EXCEPTION_TESTS_double	1
+/* compiler-rt does not support floating-point exceptions for the
+   long double / __float128 math builtins.  */
+#ifdef HAVE_COMPILER_RT_RUNTIME
+# define EXCEPTION_TESTS_long_double	0
+# define EXCEPTION_TESTS_float128	0
+#else
+# define EXCEPTION_TESTS_long_double	1
+# define EXCEPTION_TESTS_float128	1
+#endif
+
+#endif /* math-tests-exceptions.h.  */
