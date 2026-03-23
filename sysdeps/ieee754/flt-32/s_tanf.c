147635bcf663dd9b0a6072cb5134d0328c6aaee7
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2026-03-23 14:35:46 -0300

    math: Consolidate common definitions for cosf/sinf/tanf/sincosf
    The rbig does not use floating-point instructions, so it can be on a
    different TU and not be built multiple times for multiarch (since the
    resulting code will be similar and not a hotspot).
    
    Checked on x86_64-linux-gnu, i686-linux-gnu, aarch64-linux-gnu,
    and powerpc64le-linux-gnu.

--- sysdeps/ieee754/flt-32/s_tanf.c
+++ sysdeps/ieee754/flt-32/s_tanf.c
@@ -1,6 +1,6 @@
 /* Correctly-rounded tangent of binary32 value.
 
-Copyright (c) 2022-2024 Alexei Sibidanov.
+Copyright (c) 2022-2026 Alexei Sibidanov.
 
 The original version of this file was copied from the CORE-MATH
 project (file src/binary32/tan/tanf.c, revision 59d21d7).
@@ -27,13 +27,12 @@ SOFTWARE.
 #include <array_length.h>
 #include <stdint.h>
 #include <libm-alias-float.h>
-#include "math_config.h"
-#include <math_uint128.h>
+#include <s_sincosf_common.h>
 
 /* argument reduction
    for |z| < 2^28, return r such that 2/pi*x = q + r  */
-static inline double
-rltl (float z, int *q)
+static __always_inline double
+rltl_tanf (float z, int *q)
 {
   double x = z;
   double idl = -0x1.b1bbead603d8bp-32 * x;
@@ -41,58 +40,6 @@ rltl (float z, int *q)
   double id = roundeven_finite (idh);
   *q = (int64_t) id;
   return (idh - id) + idl;
-}
-
-/* argument reduction
-   same as rltl, but for |x| >= 2^28  */
-static double __attribute__ ((noinline))
-rbig (uint32_t u, int *q)
-{
-  static const uint64_t ipi[] =
-    {
-      0xfe5163abdebbc562, 0xdb6295993c439041,
-      0xfc2757d1f534ddc0, 0xa2f9836e4e441529
-    };
-  int e = (u >> 23) & 0xff, i;
-  uint64_t m = (u & (~0u >> 9)) | 1 << 23;
-  u128 p0 = u128_mul (u128_from_u64 (m), u128_from_u64 (ipi[0]));
-  u128 p1 = u128_mul (u128_from_u64 (m), u128_from_u64 (ipi[1]));
-  p1 = u128_add (p1, u128_rshift (p0, 64));
-  u128 p2 = u128_mul (u128_from_u64 (m), u128_from_u64 (ipi[2]));
-  p2 = u128_add (p2, u128_rshift (p1, 64));
-  u128 p3 = u128_mul (u128_from_u64 (m), u128_from_u64 (ipi[3]));
-  p3 = u128_add (p3, u128_rshift (p2, 64));
-  uint64_t p3h = u128_high (p3);
-  uint64_t p3l = u128_low (p3);
-  uint64_t p2l = u128_low (p2);
-  uint64_t p1l = u128_low (p1);
-  int64_t a;
-  int k = e - 127, s = k - 23;
-  /* in ctanf(), rbig() is called in the case 127+28 <= e < 0xff
-     thus 155 <= e <= 254, which yields 28 <= k <= 127 and 5 <= s <= 104 */
-  if (s < 64)
-    {
-      i = p3h << s | p3l >> (64 - s);
-      a = p3l << s | p2l >> (64 - s);
-    }
-  else if (s == 64)
-    {
-      i = p3l;
-      a = p2l;
-    }
-  else
-    { /* s > 64 */
-      i = p3l << (s - 64) | p2l >> (128 - s);
-      a = p2l << (s - 64) | p1l >> (128 - s);
-    }
-  int sgn = u;
-  sgn >>= 31;
-  int64_t sm = a >> 63;
-  i -= sm;
-  double z = (a ^ sgn) * 0x1p-64;
-  i = (i ^ sgn) - sgn;
-  *q = i;
-  return z;
 }
 
 float
@@ -111,10 +58,10 @@ __tanf (float x)
 	  float x2 = x * x;
 	  return fmaf (x, 0x1.555556p-2f * x2, x);
 	}
-      z = rltl (x, &i);
+      z = rltl_tanf (x, &i);
     }
   else if (e < 0xff)
-    z = rbig (t, &i);
+    z = RBIG_TANF (t, &i);
   else
     {
       if (t << 9)
