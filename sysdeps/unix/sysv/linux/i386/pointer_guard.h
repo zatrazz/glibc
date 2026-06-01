80f0122c41dc616eca8890da4f2ca039fdf5e4e8
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2026-06-01 15:24:28 -0300

    Consolidate pointer guard to use a relro variable instead of the TCB
    For the targets that still read the pointer guard from the thread
    descriptor (i386, x86_64, powerpc, s390, sparc, and sh), mangle and
    demangle pointers using the module-local __pointer_chk_guard_local (in
    ld.so and static binaries) or the global __pointer_chk_guard provided by
    the dynamic loader (in shared objects), matching the scheme already used
    by aarch64, alpha, arm, csky and loongarch.
    
    This removes the dependency on the TCB pointer_guard slot: drop
    THREAD_{GET,SET,COPY}_POINTER_GUARD and the POINTER_GUARD tcb-offset,
    and point the POINTER_CHK_GUARD test macro at the relro variable.  The
    tcbhead_t slot is kept (renamed to an unused reserved field, except on
    s390 which reused the stack guard) to preserve the ABI.  s390 gains a
    distinct pointer guard rather than aliasing the stack guard.
    
    Checked on x86_64-linux-gnu, aarch64-linux-gnu, i686-linux-gnu, and
    armv7a-linux-gnueabihf.  I also run elf and setjmp checks on qemu
    loongarch64, powerpc64, powerpc64le, and s390x systems.

--- sysdeps/unix/sysv/linux/i386/pointer_guard.h
+++ sysdeps/unix/sysv/linux/i386/pointer_guard.h
@@ -19,26 +19,53 @@
 #ifndef POINTER_GUARD_H
 #define POINTER_GUARD_H
 
-#include <tcb-offsets.h>
-
 #if IS_IN (rtld)
 /* We cannot use the thread descriptor because in ld.so we use setjmp
-   earlier than the descriptor is initialized.  Using a global variable
-   is too complicated here since we have no PC-relative addressing mode.  */
+   earlier than the descriptor is initialized.  */
 # include <sysdeps/generic/pointer_guard.h>
 #else
 # ifdef __ASSEMBLER__
-#  define PTR_MANGLE(reg)	xorl %gs:POINTER_GUARD, reg;		      \
+#  include <sysdep.h>
+#  ifdef SHARED
+/* The global guard is reached through the GOT.  setjmp/__longjmp save and
+   restore %ebx and %esi around the mangling, so they can be used here as
+   the GOT pointer and a scratch register.  */
+#   define PTR_MANGLE(reg)	LOAD_PIC_REG (bx);			      \
+				movl __pointer_chk_guard@GOT(%ebx), %esi;     \
+				xorl (%esi), reg;			      \
 				roll $9, reg
-#  define PTR_DEMANGLE(reg)	rorl $9, reg;				      \
-				xorl %gs:POINTER_GUARD, reg
+#   define PTR_DEMANGLE(reg)	rorl $9, reg;				      \
+				LOAD_PIC_REG (bx);			      \
+				movl __pointer_chk_guard@GOT(%ebx), %esi;     \
+				xorl (%esi), reg
+#  elif defined PIC
+/* Static PIE: the module-local guard is reached via @GOTOFF.  */
+#   define PTR_MANGLE(reg)	LOAD_PIC_REG (bx);			      \
+				xorl __pointer_chk_guard_local@GOTOFF(%ebx), reg; \
+				roll $9, reg
+#   define PTR_DEMANGLE(reg)	rorl $9, reg;				      \
+				LOAD_PIC_REG (bx);			      \
+				xorl __pointer_chk_guard_local@GOTOFF(%ebx), reg
+#  else
+/* Position-dependent code addresses the guard directly.  */
+#   define PTR_MANGLE(reg)	xorl __pointer_chk_guard_local, reg;	      \
+				roll $9, reg
+#   define PTR_DEMANGLE(reg)	rorl $9, reg;				      \
+				xorl __pointer_chk_guard_local, reg
+#  endif
 # else
 #  include <stdbit.h>
-#  include <tls.h>
+#  include <stdint.h>
+#  ifdef SHARED
+extern uintptr_t __pointer_chk_guard attribute_relro;
+#   define PTR_GUARD_VALUE	__pointer_chk_guard
+#  else
+extern uintptr_t __pointer_chk_guard_local attribute_relro attribute_hidden;
+#   define PTR_GUARD_VALUE	__pointer_chk_guard_local
+#  endif
 #  define PTR_MANGLE(var)						      \
     do {								      \
-      (var) = (__typeof (var)) ((uintptr_t) (var)			      \
-				^ ((tcbhead_t __seg_gs *)0)->pointer_guard);  \
+      (var) = (__typeof (var)) ((uintptr_t) (var) ^ PTR_GUARD_VALUE);	      \
       (var) = (__typeof (var)) stdc_rotate_left ((uintptr_t) (var),	      \
 						 2 * sizeof (uintptr_t) + 1); \
     } while (0)
@@ -46,8 +73,7 @@
     do {								      \
       (var) = (__typeof (var)) stdc_rotate_right ((uintptr_t) (var),	      \
 						  2 * sizeof (uintptr_t) + 1); \
-      (var) = (__typeof (var)) ((uintptr_t) (var)			      \
-				^ ((tcbhead_t __seg_gs *)0)->pointer_guard);  \
+      (var) = (__typeof (var)) ((uintptr_t) (var) ^ PTR_GUARD_VALUE);	      \
     } while (0)
 # endif
 #endif
