3db078d7359faecd95760f7a18caae91de6031aa
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2026-07-08 13:22:02 -0300

    math: Make s_sincos.c independent of s_sin.c
    s_sincos.c reused the sin/cos helper routines (do_sin, do_cos,
    reduce_sincos, do_sincos and the shared lookup table) by including
    s_sin.c with IN_SINCOS defined.  To allow replacing s_sin.c with the
    CORE-MATH sin implementation independently, keep a private copy of
    those helpers in s_sincos.c and drop the include of s_sin.c.
    
    This is a pure code move, no functional change.  Checked on
    x86_64-linux-gnu.

--- sysdeps/ieee754/dbl-64/s_sincos.c
+++ sysdeps/ieee754/dbl-64/s_sincos.c
@@ -28,8 +28,165 @@
 # define SECTION
 #endif
 
-#define IN_SINCOS
-#include "s_sin.c"
+/* The following helpers (do_sin, do_cos, reduce_sincos, do_sincos and the
+   shared sin/cos lookup table) were previously shared with s_sin.c via
+   #include; they are kept here so sincos is independent of the sin
+   implementation.  */
+#include <errno.h>
+#include <float.h>
+#include "endian.h"
+#include "mydefs.h"
+#include "usncs.h"
+#include <math.h>
+#include <math_private.h>
+#include <fenv_private.h>
+#include <math-underflow.h>
+#include <libm-alias-double.h>
+#include <fenv.h>
+
+/* Helper macros to compute sin of the input values.  */
+#define POLYNOMIAL2(xx) ((((s5 * (xx) + s4) * (xx) + s3) * (xx) + s2) * (xx))
+
+#define POLYNOMIAL(xx) (POLYNOMIAL2 (xx) + s1)
+
+/* The computed polynomial is a variation of the Taylor series expansion for
+   sin(x):
+
+   x - x^3/3! + x^5/5! - x^7/7! + x^9/9! - dx*x^2/2 + dx
+
+   The constants s1, s2, s3, etc. are pre-computed values of 1/3!, 1/5! and so
+   on.  The result is returned to LHS.  */
+#define TAYLOR_SIN(xx, x, dx) \
+({									      \
+  double t = ((POLYNOMIAL (xx)  * (x) - 0.5 * (dx))  * (xx) + (dx));	      \
+  double res = (x) + t;							      \
+  res;									      \
+})
+
+#define SINCOS_TABLE_LOOKUP(u, sn, ssn, cs, ccs) \
+({									      \
+  int4 k = u.i[LOW_HALF] << 2;						      \
+  sn = __sincostab.x[k];						      \
+  ssn = __sincostab.x[k + 1];						      \
+  cs = __sincostab.x[k + 2];						      \
+  ccs = __sincostab.x[k + 3];						      \
+})
+
+#ifndef SECTION
+# define SECTION
+#endif
+
+extern const union
+{
+  int4 i[880];
+  double x[440];
+} __sincostab attribute_hidden;
+
+static const double
+  sn3 = -1.66666666666664880952546298448555E-01,
+  sn5 = 8.33333214285722277379541354343671E-03,
+  cs2 = 4.99999999999999999999950396842453E-01,
+  cs4 = -4.16666666666664434524222570944589E-02,
+  cs6 = 1.38888874007937613028114285595617E-03;
+
+int __branred (double x, double *a, double *aa);
+
+/* Given a number partitioned into X and DX, this function computes the cosine
+   of the number by combining the sin and cos of X (as computed by a variation
+   of the Taylor series) with the values looked up from the sin/cos table to
+   get the result.  */
+static __always_inline double
+do_cos (double x, double dx)
+{
+  mynumber u;
+
+  if (x < 0)
+    dx = -dx;
+
+  u.x = big + fabs (x);
+  x = fabs (x) - (u.x - big) + dx;
+
+  double xx, s, sn, ssn, c, cs, ccs, cor;
+  xx = x * x;
+  s = x + x * xx * (sn3 + xx * sn5);
+  c = xx * (cs2 + xx * (cs4 + xx * cs6));
+  SINCOS_TABLE_LOOKUP (u, sn, ssn, cs, ccs);
+  cor = (ccs - s * ssn - cs * c) - sn * s;
+  return cs + cor;
+}
+
+/* Given a number partitioned into X and DX, this function computes the sine of
+   the number by combining the sin and cos of X (as computed by a variation of
+   the Taylor series) with the values looked up from the sin/cos table to get
+   the result.  */
+static __always_inline double
+do_sin (double x, double dx)
+{
+  double xold = x;
+  /* Max ULP is 0.501 if |x| < 0.126, otherwise ULP is 0.518.  */
+  if (fabs (x) < 0.126)
+    return TAYLOR_SIN (x * x, x, dx);
+
+  mynumber u;
+
+  if (x <= 0)
+    dx = -dx;
+  u.x = big + fabs (x);
+  x = fabs (x) - (u.x - big);
+
+  double xx, s, sn, ssn, c, cs, ccs, cor;
+  xx = x * x;
+  s = x + (dx + x * xx * (sn3 + xx * sn5));
+  c = x * dx + xx * (cs2 + xx * (cs4 + xx * cs6));
+  SINCOS_TABLE_LOOKUP (u, sn, ssn, cs, ccs);
+  cor = (ssn + s * ccs - sn * c) + cs * s;
+  return copysign (sn + cor, xold);
+}
+
+/* Reduce range of x to within PI/2 with abs (x) < 105414350.  The high part
+   is written to *a, the low part to *da.  Range reduction is accurate to 136
+   bits so that when x is large and *a very close to zero, all 53 bits of *a
+   are correct.  */
+static __always_inline int4
+reduce_sincos (double x, double *a, double *da)
+{
+  mynumber v;
+
+  double t = (x * hpinv + toint);
+  double xn = t - toint;
+  v.x = t;
+  double y = (x - xn * mp1) - xn * mp2;
+  int4 n = v.i[LOW_HALF] & 3;
+
+  double b, db, t1, t2;
+  t1 = xn * pp3;
+  t2 = y - t1;
+  db = (y - t2) - t1;
+
+  t1 = xn * pp4;
+  b = t2 - t1;
+  db += (t2 - b) - t1;
+
+  *a = b;
+  *da = db;
+  return n;
+}
+
+/* Compute sin or cos (A + DA) for the given quadrant N.  */
+static __always_inline double
+do_sincos (double a, double da, int4 n)
+{
+  double retval;
+
+  if (n & 1)
+    /* Max ULP is 0.513.  */
+    retval = do_cos (a, da);
+  else
+    /* Max ULP is 0.501 if xx < 0.01588, otherwise ULP is 0.518.  */
+    retval = do_sin (a, da);
+
+  return (n & 2) ? -retval : retval;
+}
 
 void
 SECTION
