c7f0d4d9509483927b344740c776b675cf3414a3
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2026-09-20 13:32:45 -0500

    math: Consolidate the exp2m1/exp10m1 common data
    exp2m1 and exp10m1 carry an identical copy of the two tables used by the
    double-double exp() they are built on: the double-double approximations
    of 2^(i/64) and of 2^(i/2^12).  Move them to a new translation unit.
    
    No functional change.
    
    Checked on aarch64-linux-gnu.

---
+++ sysdeps/ieee754/dbl-64/s_exp2m1_common.h
@@ -0,0 +1,41 @@
+/* Common data for the binary64 exp2m1 and exp10m1 implementations.
+
+Copyright (c) 2022-2025 Alexei Sibidanov, Paul Zimmermann, Tom Hubrecht and
+Claude-Pierre Jeannerod.
+
+The original version of this file was copied from the CORE-MATH
+project (file src/binary64/exp2m1/exp2m1.c, revision 8ea8ea35).
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#ifndef _EXP2M1_COMMON_H
+#define _EXP2M1_COMMON_H
+
+#include <math.h>
+#include <stdint.h>
+#include "math_config.h"
+
+/* For 0 <= i < 64, T1[i] = (h,l) such that h+l is the best double-double
+   approximation of 2^(i/64), and T2[i] likewise for 2^(i/2^12).  The
+   approximation error is bounded by 2^-107.  */
+extern const double __exp2m1_t1[64][2] attribute_hidden;
+extern const double __exp2m1_t2[64][2] attribute_hidden;
+
+#endif
